3dd041264442fa340e1e74088f8bbf736e3b471f
bnagel <nagelb500@gmail.com>
2022-11-21 14:05:03 -0500

    initial page setup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+
+function Welcome(props) {
+  return <h1>Welcome, {props.name}!</h1>;
+}
+
+function Clock(props) {
+  return <h5> It is {props.time}.</h5>
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div>
+            <Welcome name="Hero" />
+          </div>
       </header>
+      <footer className="App-footer">
+          <div>
+            <Clock time={new Date().toLocaleTimeString()} />
+          </div>
+      </footer>
     </div>
   );
 }
